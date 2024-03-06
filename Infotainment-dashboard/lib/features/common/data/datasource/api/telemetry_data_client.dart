36e2911c1f5180fe4e666e9794412f5fd72b785f
Fran Cudina <cudina.fran@gmail.com>
2024-03-06 20:21:23 +0100

    retry iterations

--- Infotainment-dashboard/lib/features/common/data/datasource/api/telemetry_data_client.dart
+++ Infotainment-dashboard/lib/features/common/data/datasource/api/telemetry_data_client.dart
@@ -1,19 +1,28 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class TelemetryDataClient {
   final String address;
   late WebSocketChannel channel;
+  late IOSink _logSink;
 
-  TelemetryDataClient(this.address);
+  TelemetryDataClient(this.address) {
+    _initializeLogging();
+  }
 
-  // Stream<dynamic> listen() {
-  //   final uri = Uri.parse('ws://$address');
-  //   channel = WebSocketChannel.connect(uri);
-  //   return channel.stream;
-  // }
+  void _initializeLogging() {
+    File logFile = File('telemetry_data_client.log');
+    _logSink = logFile.openWrite(mode: FileMode.append);
+  }
+
+  void _log(String message) {
+    _logSink.writeln('[${DateTime.now()}] $message');
+  }
 
   Stream<dynamic> listen() {
+    _log('Listening on websocket address $address');
+
     StreamController<dynamic> controller = StreamController<dynamic>();
 
     void connectWithRetry(int retryCount) {
@@ -26,15 +35,17 @@ class TelemetryDataClient {
         onError: (error) {
           if (retryCount < 1000) {
             Future.delayed(Duration(seconds: 1), () {
-              print('Retrying to Connect to websocket $address, count: $retryCount');
+              _log('Retrying to Connect to websocket $address, count: $retryCount');
               connectWithRetry(retryCount + 1);
             });
           } else {
+            _log('Connection to websocket $address failed after 1000 retries: $error');
             controller.addError(error);
             controller.close();
           }
         },
         onDone: () {
+          _log('Connection to websocket $address closed.');
           controller.close();
         },
         cancelOnError: true,
@@ -46,6 +57,9 @@ class TelemetryDataClient {
     return controller.stream;
   }
 
-
-  void disconnect() => channel.sink.close();
+  void disconnect() {
+    channel.sink.close();
+    _log('Disconnected from websocket $address.');
+    _logSink.close();
+  }
 }
