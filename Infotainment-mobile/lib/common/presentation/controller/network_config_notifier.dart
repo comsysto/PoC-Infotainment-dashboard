0a8c881b33312edb74e4a2fb5a01f42706df6070
FilipKisic <filip.kisic@racunarstvo.hr>
2024-03-22 13:09:56 +0100

    Updated ip address

--- Infotainment-mobile/lib/common/presentation/controller/network_config_notifier.dart
+++ Infotainment-mobile/lib/common/presentation/controller/network_config_notifier.dart
@@ -2,11 +2,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class NetworkConfigNotifier extends Notifier<String> {
   @override
-  build() {
-    return '192.168.14.144:56035';
-  }
+  build() => '10.100.3.90:56035';
 
   void setConfig({final String? ipAddress, final String? port}) {
-    state = '${ipAddress ?? '10.100.3.72'}:${port ?? '56035'}';
+    state = '${ipAddress ?? '10.100.3.90'}:${port ?? '56035'}';
   }
 }
